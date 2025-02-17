b19b4e11ce688a1c633c2265027d48d1ae908562
yashkherodiya <yashkherodiya@gmail.com>
2025-02-17 22:11:11 +0530

    Create general1.cpp

---
+++ general1.cpp
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="/action">
+<h2>Feedback Form</h2>
+<label for="name">Name:</label>
+<input type="text" name="name" id="name" placeholder="Write your name">
+<br><br>
+<label for="email">Email:</label>
+<input type="text" name="email" id="email" placeholder="Write your email">
+<br><br>
+<label for="txt">Message:</label><br>
+<textarea name="txt" id="txt" cols="30" raw="4"></textarea>
+<br><br>
+Rate us our of 5:
+<br>
+<input type="radio" name="rad" id="rad1">
+<label for="rad1">1</label><br>
+<input type="radio" name="rad" id="rad2">
+<label for="rad2">2</label><br>
+<input type="radio" name="rad" id="rad3">
+<label for="rad3">3</label><br>
+<input type="radio" name="rad" id="rad4">
+<label for="rad4">4</label><br>
+<input type="radio" name="rad" id="rad5">
+<label for="rad5">5</label>
+<br><br>
+<button>Send your message!</button>
+</form>
+</body>
+</html>
